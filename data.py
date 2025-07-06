b01e4c86bdde5db90d605618795de6a4e186eb0d
Ghulam Latif <141661326+ashahmir@users.noreply.github.com>
2025-07-06 22:26:11 +0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,42 @@
+question_data = [
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Video Games",
+     "question": "Amazon acquired Twitch in August 2014 for $970 million dollars.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+     "question": "The vapor produced by e-cigarettes is actually water.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Mythology",
+     "question": "The Roman god Jupiter was first known as Zeus to the Greeks.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Geography",
+     "question": "There exists an island named Java.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "History",
+     "question": "Brezhnev was the 5th leader of the USSR.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Animals",
+     "question": "Finnish Lapphund dogs were used for herding reindeer.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+     "question": "Popcorn was invented in 1871 by Frederick W. Rueckheim in the USA where he sold "
+                 "the snack on the streets of Chicago.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "General Knowledge",
+     "question": "Kraft Cheez Whiz contains cheese culture, but doesn't actually contain cheese",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Japanese Anime &amp; Manga",
+     "question": "In Full Metal Panic!, Whispered are those who are capable of creating Black Technology.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+
+    {"type": "boolean", "difficulty": "medium", "category": "Entertainment: Video Games",
+     "question": "In Call Of Duty: Zombies, you can upgrade the Apothicon Servant in the Shadows Of Evil map.",
+     "correct_answer": "False", "incorrect_answers": ["True"]}
+]
